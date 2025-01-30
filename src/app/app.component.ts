c1cdac89aaf428aaa77a7b7f4bc7145b8b95910a
yakatyansh <ykpetrolhead08@outlook.com>
2025-01-31 00:29:26 +0530

    added the base layout for workout list hardcoded the workouts for now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddWorkoutComponent } from './components/add-workout/add-workout.component';
+import { WorkoutListComponent } from "./components/workout-list/workout-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AddWorkoutComponent],
+  imports: [AddWorkoutComponent, WorkoutListComponent],
   template: ` <div>
       <app-add-workout></app-add-workout>
+      <app-workout-list></app-workout-list>
     </div>`,
   styleUrl: './app.component.css'
 })
--- src/app/components/workout-list/workout-list.component.ts
+++ src/app/components/workout-list/workout-list.component.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-workout-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule], // ✅ Import FormsModule for ngModel
   templateUrl: './workout-list.component.html',
 })
 export class WorkoutListComponent {
+  searchTerm: string = ''; // This binds to ngModel
+  workoutTypeFilter: string = '';
 
+  workouts = [
+    { UserName: 'Yash', workoutType: 'Running', workoutMinutes: 30 },
+    { UserName: 'John', workoutType: 'Cycling', workoutMinutes: 45 },
+    { UserName: 'Alice', workoutType: 'Yoga', workoutMinutes: 60 },
+  ];
+  filteredWorkouts = [...this.workouts]; 
+
+  filterWorkout(type: string) {
+    this.filteredWorkouts = this.workouts.filter(workout => workout.workoutType === type);
+  }
 }
